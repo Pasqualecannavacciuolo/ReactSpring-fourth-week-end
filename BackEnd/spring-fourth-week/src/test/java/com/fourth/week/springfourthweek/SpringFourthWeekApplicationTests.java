2e3f7665cce88fc7d99cbf65f71c9941c73eeb9e
Pasquale Cannavacciuolo <48246188+Pasqualecannavacciuolo@users.noreply.github.com>
2022-04-17 17:00:47 +0200

    Added Backend

---
+++ BackEnd/spring-fourth-week/src/test/java/com/fourth/week/springfourthweek/SpringFourthWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourth.week.springfourthweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFourthWeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
